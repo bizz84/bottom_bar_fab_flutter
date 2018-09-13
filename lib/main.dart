2125a78b72653b039a4eb35572722e1c88a68448
Andrea Bizzotto <bizz84@gmail.com>
2018-09-13 14:51:34 +0100

    Update title

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'BottomAppBar with FAB'),
     );
   }
 }
